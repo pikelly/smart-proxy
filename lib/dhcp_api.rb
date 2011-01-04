aa57bc9ae8d952870d99e189dd481cb4b9b60ba7
Paul Kelly <pikelly@blueyonder.co.uk>
2011-01-04 17:02:48 +0000

    Fixes #64 - Adding native MS DHCP server support

--- lib/dhcp_api.rb
+++ lib/dhcp_api.rb
@@ -1,96 +1,107 @@
-def dhcp_setup
-  raise "Smart Proxy is not configured to support DHCP" unless SETTINGS.dhcp
-  case SETTINGS.dhcp_vendor
-  when "isc"
-    require 'proxy/dhcp/server/isc'
-    config   = File.read(SETTINGS.dhcp_config)
-    leases   = File.read(SETTINGS.dhcp_leases)
-    @server  = Proxy::DHCP::ISC.new({:name => "127.0.0.1", :config => config, :leases =>leases})
-  else
-    raise "Unsupported DHCP server #{SETTINGS.dhcp_vendor}"
-  end
-  @subnets = @server.subnets
-rescue => e
-  logger.warn "unable to process something: #{e}"
-  halt 400, e.to_s
-end
+class SmartProxy
 
-helpers do
-  def load_subnet
-    @subnet  = @server.find_subnet(params[:network])
-    halt 404, "Subnet #{params[:network]} not found" unless @subnet
-    @subnet
-  end
-end
-
-before do
-  dhcp_setup if request.path_info =~ /dhcp/
-end
-
-get "/dhcp" do
-  begin
-    if request.accept.include?("application/json")
-      content_type :json
-
-      halt 404 unless @subnets
-      @subnets.map{|s| {:network => s.network, :netmask => s.netmask }}.to_json
+  def dhcp_setup
+    # We allow an override of the configuration setting via a url option. This will be remembered by the proxy.
+    SETTINGS.server = params[:server] if params[:server]
+    if SETTINGS.dhcp_vendor.downcase == "native_ms"
+      require 'proxy/dhcp/server/native_ms'
+      @server = Proxy::DHCP.server || Proxy::DHCP::NativeMS.new(:server => SETTINGS.dhcp_server ? SETTINGS.dhcp_server : "127.0.0.1")
+    elsif SETTINGS.dhcp_vendor.downcase == "isc"
+      require 'proxy/dhcp/server/isc'
+      log_halt 400, "Unable to find the lease files" unless SETTINGS.dhcp_config and SETTINGS.dhcp_leases and File.exist?(SETTINGS.dhcp_config) and File.exist?(SETTINGS.dhcp_leases)
+      config=File.read(SETTINGS.dhcp_config)
+      leases=File.read(SETTINGS.dhcp_leases)
+      @server = Proxy::DHCP.server || Proxy::DHCP::ISC.new(:name => "127.0.0.1", :config => config, :leases =>leases)
     else
-      haml :"dhcp/index"
+      log_halt 400, "Unrecognised or missing DHCP vendor type: #{SETTINGS.dhcp_vendor.nil? ? "MISSING" : SETTINGS.dhcp_vendor}"
     end
+    Proxy::DHCP.server = @server if SETTINGS.dhcp_cache_enabled
+    @subnets = @server.subnets
   rescue => e
-    halt 400, e.to_s
+    log_halt 400, e.to_s
   end
-end
 
-get "/dhcp/:network" do
-  begin
-    load_subnet
-    haml :"dhcp/show"
-  rescue => e
-    halt 400, e.to_s
+  helpers do
+    def log_halt code, message
+      logger.error message
+      halt code, message
+    end
+
+    def load_subnet
+      @subnet  = @server.find_subnet(params[:network])
+      log_halt 404, "Subnet #{params[:network]} not found" unless @subnet
+      @subnet
+    end
   end
-end
 
-get "/dhcp/:network/unused_ip" do
-  begin
-    load_subnet.unused_ip
-  rescue => e
-    halt 400, e.to_s
+  before do
+    dhcp_setup if request.path_info =~ /dhcp/
   end
-end
 
-get "/dhcp/:network/:record" do
-  begin
-    content_type :json
-    record = load_subnet[params[:record]]
-    halt 404, "Record #{params[:network]}/#{params[:record]} not found" unless record
-    record.options.to_json
-  rescue => e
-    halt 400, e.to_s
+  get "/dhcp" do
+    begin
+      if request.accept.include?("application/json")
+        content_type :json
+
+        log_halt 404, "No subnets found on server @{name}" unless @subnets
+        @subnets.map{|s| {:network => s.network, :netmask => s.netmask }}.to_json
+      else
+        haml :"dhcp/index"
+      end
+    rescue => e
+      log_halt 400, e.to_s
+    end
   end
-end
 
-# create a new record in a network
-post "/dhcp/:network" do
-  begin
-    content_type :json
-    halt 400, "Record #{params[:network]}/#{params[:ip]} already exists" if @server.find_record(params[:ip])
-    @server.addRecord({ :mac=> params[:mac], :nextserver=> params[:nextserver],
-                      :hostname=>params[:name], :filename=> params[:filename],
-                      :name=>params[:name], :ip=>params[:ip]})
-  rescue => e
-    logger.warn "Failed to process request - #{e}"
-    halt 400, e.to_s
+  get "/dhcp/:network" do
+    begin
+      load_subnet
+      haml :"dhcp/show"
+    rescue => e
+      log_halt 400, e.to_s
+    end
   end
-end
 
-# delete a record from a network
-delete "/dhcp/:network/:record" do
-  begin
-    record = load_subnet[params[:record]]
-    halt 404, "Record #{params[:network]}/#{params[:record]} not found" unless record
-    @server.delRecord @subnet, record
-  rescue Exception => e
-    halt 400, e.to_s
+  get "/dhcp/:network/unused_ip" do
+    begin
+      load_subnet.unused_ip
+    rescue => e
+      log_halt 400, e.to_s
+    end
+  end
+
+  get "/dhcp/:network/:record" do
+    begin
+      content_type :json
+      record = load_subnet[params[:record]]
+      log_halt 404, "Record #{params[:network]}/#{params[:record]} not found" unless record
+      record.options.to_json
+    rescue => e
+      log_halt 400, e.to_s
+    end
+  end
+
+  # create a new record in a network
+  post "/dhcp/:network" do
+    begin
+      content_type :json
+      log_halt 400, "Record #{params[:network]}/#{params[:ip]} already exists" if @server.find_record(params[:ip])
+      @server.addRecord({ :mac=> params[:mac], :nextserver=> params[:nextserver],
+                          :hostname=>params[:name], :filename=> params[:filename],
+                          :name=>params[:name], :ip=>params[:ip]})
+    rescue => e
+      log_halt 400, e.to_s
+    end
+  end
+
+  # delete a record from a network
+  delete "/dhcp/:network/:record" do
+    begin
+      record = load_subnet[params[:record]]
+      log_halt 404, "Record #{params[:network]}/#{params[:record]} not found" unless record
+      @server.delRecord @subnet, record
+    rescue Exception => e
+      log_halt 400, e.to_s
+    end
   end
 end
